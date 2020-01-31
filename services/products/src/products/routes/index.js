bb86c3186f020e28b48620e9895a1c5ecf33d3f4
eclesiomelojunior <eclesiomelo.1@gmail.com>
2020-01-31 15:53:13 -0400

    update products routes

--- services/products/src/products/routes/index.js
+++ services/products/src/products/routes/index.js
@@ -4,11 +4,11 @@ const {
 } = require('../controllers');
 
 const routes = {
-  '/v1/products/addProduct': {
+  '/v1/products': {
     method: 'post',
     controller: addProductController,
   },
-  '/v1/products/getProduct': {
+  '/v1/products/:id': {
     method: 'get',
     controller: getProductByIdController,
   },
--- services/products/src/products/useCases/addProduct.js
+++ services/products/src/products/useCases/addProduct.js
@@ -1,9 +1,15 @@
+const {
+  product,
+} = require('../domain');
+
 /**
  * @return {function} addProduct is a function to insert a product to inventory
  */
 function buildAddProduct() {
-  return async function addProduct() {
+  return async function addProduct(newProduct) {
+    const productModel = product(newProduct);
 
+    console.log(productModel);
   };
 }
 
